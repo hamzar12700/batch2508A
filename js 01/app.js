05a9ee2cc590506b33ee4464317aa92230028114
Hamza Raza <hr124055@gmail.com>
2026-02-21 10:20:18 +0500

    first commit

---
+++ js 01/app.js
@@ -0,0 +1,63 @@
+console.log("js file connected");
+
+
+
+// var std = ["Aslam","Salman","Shakoor","Ghafoor", "Shahrukh", "Amir", "Taha","Usman"];
+
+
+// for(var i =0 ; i < std.length ;i++){
+//     console.log(std[i]);
+
+// }
+
+
+// console.log("Bache apni jagha se utho");
+// console.log("cabnet ko kholo or usme se glass ko nikalo");
+// console.log("kooolar ki tooti ko dabao or jese hi usme pani nikle to apna glass rkh dena");
+// console.log("jese hi pani bhar jaye to tooti ko band krdena");
+// console.log("mujhe lake de dena");
+
+// console.log("Bache apni jagha se utho");
+// console.log("cabnet ko kholo or usme se glass ko nikalo");
+// console.log("kooolar ki tooti ko dabao or jese hi usme pani nikle to apna glass rkh dena");
+// console.log("jese hi pani bhar jaye to tooti ko band krdena");
+// console.log("mujhe lake de dena");
+
+
+
+
+function paniLado() {
+    console.log("Bache apni jagha se utho");
+    console.log("cabnet ko kholo or usme se glass ko nikalo");
+    console.log("kooolar ki tooti ko dabao or jese hi usme pani nikle to apna glass rkh dena");
+    console.log("jese hi pani bhar jaye to tooti ko band krdena");
+    console.log("mujhe lake de dena");
+}
+
+
+// paniLado()
+// paniLado()
+
+
+
+
+function sum (a,b=5){
+    console.log(a+b);
+}
+
+
+sum(3,8)
+
+
+
+
+
+
+
+
+
+
+// console.log(std[1] , std[2]);
+// console.log();
+// console.log(std[3]);
+// console.log(std[4]);
